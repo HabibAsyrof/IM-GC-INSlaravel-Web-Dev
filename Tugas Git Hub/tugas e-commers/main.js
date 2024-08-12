a1fbf7d5fda9883465c69b6089266357eb6bdd0e
HabibAsyrof <habibasyrof2006@icloud.com>
2024-08-12 14:53:27 +0700

    tugas baru

--- Tugas Git Hub/tugas e-commers/main.js
+++ Tugas Git Hub/tugas e-commers/main.js
@@ -11,20 +11,26 @@ const cart = document.getElementById("cart")
 const data = [
     {
         gambar: '/Tugas Git Hub/tugas e-commers/asset/logitek.webp',
-        nama: 'logitek',
+        nama: 'Keyboard Logitek',
         deskripsi: 'Keyboard gaming berkualitas',
         harga: 'Rp 60.000,00'
     },
     {
-        gambar: '/Tugas Git Hub/tugas e-commers/asset/logitek.webp',
-        nama: 'Asus',
-        deskripsi: 'Keyboard gaming berkualitas',
+        gambar: 'https://www.memoryc.com/images/products/bb/image_6244734.jpg',
+        nama: 'Mouse Hyper X',
+        deskripsi: 'Mouse gaming berkualitas',
         harga: 'Rp 60.000,00'
     },
     {
-        gambar: '/Tugas Git Hub/tugas e-commers/asset/logitek.webp',
-        nama: 'Lenovo',
-        deskripsi: 'Keyboard gaming berkualitas',
+        gambar: 'https://hotline.ua/img/tx/378/3789380085.jpg',
+        nama: 'Asus Gaming',
+        deskripsi: 'laptop dengan spek gaming',
+        harga: 'Rp 60.000,00'
+    },
+    {
+        gambar: 'https://lh5.googleusercontent.com/proxy/kL6ilxhY9iwE2Gjh8dDl-H_0q5GWscUsOisEcj_Dhkhv24-mKDuOVdt0H07zxTYYj7NwF03mtPPnvk7b9_Idj-SxaDdLVtZ8JNKrKIJ9ajU952cVzTkiwh2CqoAq7YD1LEC20nETiQt6-IL8oxGoTuF1xJBEEHwatrZtntyay-Ry=w1200-h630-p-k-no-nu',
+        nama: 'Headphone',
+        deskripsi: 'Headphone gaming yang berkualitas',
         harga: 'Rp 60.000,00'
     },
 
